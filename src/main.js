d4cdd23db6369ddb27465a7236f3214c82c6fa16
unknown <zf7067@HKGY170128.hikyjy.com.cn>
2019-02-25 17:38:27 +0800

    190225 g

--- src/main.js
+++ src/main.js
@@ -15,10 +15,38 @@ import './config/rem'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import wxShare from './utils/wxShare'
 
 Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
+
+// 步骤一 引入 微信 jssdk
+let appInit = 0
+import wx from 'weixin-js-sdk'
+Vue.use(wx)
+wxShare.config.jsSDKAuth = '/wxconfig'
+Vue.prototype.$wxShare = wxShare
+
+// 第二步 初始化微信分享
+if (location.host !== 'localhost:8080') {
+  const sign_url = location.href.split('#')[0]
+  appInit++
+  if (wx) {
+    // 初始化微信分享
+    wxShare.initWxShare(sign_url)
+  }
+}
+
+// 第三步 监听路由重置微信分享为默认
+router.afterEach(route => {
+  // let url = location.href.split('#')[0]
+  if (!store) return
+  if (appInit > 1) {
+    wxShare.resetWxShareConfig()
+  }
+  appInit++
+})
 
 new Vue({
   el: '#app',
